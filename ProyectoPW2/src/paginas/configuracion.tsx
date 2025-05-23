91889bbd170989377e2ebc8a958776473b171b14
luisgc23 <gutierrezcadilloluis@gmail.com>
2025-05-22 20:47:32 -0500

    navbar en todo el proyecto

--- ProyectoPW2/src/paginas/configuracion.tsx
+++ ProyectoPW2/src/paginas/configuracion.tsx
@@ -1,34 +1,36 @@
 import '../paginas/style.css';
-
+import Navbar from '../componentes/Navbar';
 
 const configuracion = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="container mt-5">
+        <h1>Account Settings</h1>
+        <form>
+          <div className="col-md-3">
+            <label className="form-label">Cambiar nombre</label>
+            <input type="text" className="form-control mb-3" placeholder="Pancho" />
+          </div>
 
+          <div className="col-md-3">
+            <label className="form-label">Cambiar correo electrónico</label>
+            <input type="email" className="form-control mb-3" placeholder="asd@gmail.com" />
+          </div>
+          <div className="col-md-3">
+            <label className="form-label"> Cambiar contraseña</label>
+            <input type="password" className="form-control mb-3" placeholder="*****" />
+          </div>
+          <div className="col-md-3">
+            <label className="form-label"> Cambiar pais</label>
+            <input type="text" className="form-control mb-3" placeholder="Peru" />
+          </div>
 
-return<div className="container mt-5">
-      <h1>Account Settings</h1>
-      <form>
-        <div className="col-md-3">
-          <label  className="form-label">Cambiar nombre</label>
-          <input type="text" className="form-control mb-3" placeholder="Pancho"/>
-        </div>
+          <button className="btn btn-success">Guardar Cambios</button>
+        </form>
+      </div>
+    </>
+  );
+};
 
-        <div className="col-md-3">
-          <label className="form-label">Cambiar correo electrónico</label>
-          <input type="email" className="form-control mb-3" placeholder="asd@gmail.com"/>
-        </div>
-        <div className="col-md-3">
-          <label className="form-label"> Cambiar contraseña</label>
-          <input type="password" className="form-control mb-3" placeholder="*****"/>
-        </div>
-        <div className="col-md-3">
-          <label className="form-label"> Cambiar pais</label>
-          <input type="text" className="form-control mb-3" placeholder="Peru"/>
-        </div>
-
-        <button className="btn btn-success">Guardar Cambios</button>
-      </form>
-    </div>
-}
-
-
-export default configuracion
+export default configuracion;
